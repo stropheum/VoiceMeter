32603b5fdd7af53629c59be4fa5f75f7325a8644
Dale Diaz <stropheum060@gmail.com>
2023-05-16 22:40:51 -0400

    added renderquad method

--- Assets/Scripts/StreamVisualizer.cs
+++ Assets/Scripts/StreamVisualizer.cs
@@ -1,47 +1,52 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 using VoiceMeter.Discord;
 
 namespace VoiceMeter
 {
     [RequireComponent(typeof(CanvasRenderer))]
-    public class VoiceStreamVisualizer : Graphic
+    public class StreamVisualizer : Graphic
     {
         [SerializeField] private DiscordVoiceListener _discordVoiceListener;
+
+        private List<UIVertex> _vertices = new();
+        private List<Vector3> _triangles = new();
 
         protected override void Awake()
         {
             base.Awake();
-            Debug.Assert(_discordVoiceListener != null);
         }
         
         protected override void OnPopulateMesh(VertexHelper vh)
         {
             vh.Clear();
+            RenderQuad(vh);
+        }
 
-            // Create vertices and colors
+        private void RenderQuad(VertexHelper vh)
+        {
             Vector2 size = rectTransform.rect.size;
             Vector2 pivot = rectTransform.pivot;
             UIVertex vertex = UIVertex.simpleVert;
 
-            // Set vertex positions and colors
             vertex.position = new Vector2(-pivot.x * size.x, -pivot.y * size.y);
-            vertex.color = Color.red;
+            vertex.color = Color.black;
             vh.AddVert(vertex);
-
+            
             vertex.position = new Vector2(-pivot.x * size.x, (1f - pivot.y) * size.y);
-            vertex.color = Color.green;
+            vertex.color = Color.blue;
             vh.AddVert(vertex);
-
+            
             vertex.position = new Vector2((1f - pivot.x) * size.x, (1f - pivot.y) * size.y);
             vertex.color = Color.blue;
             vh.AddVert(vertex);
-
+            
             vertex.position = new Vector2((1f - pivot.x) * size.x, -pivot.y * size.y);
-            vertex.color = Color.white;
+            vertex.color = Color.black;
             vh.AddVert(vertex);
-
-            // Create triangles
+            
             vh.AddTriangle(0, 1, 2);
             vh.AddTriangle(2, 3, 0);
         }
